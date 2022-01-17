14001427126c216e32959552d57afb2ec7f33fac
ignatown <ignatow.n@mail.ru>
2022-01-17 05:08:12 +0300

    init

---
+++ src/main/java/ru/job4j/passportclient/PassportManagementClientApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.passportclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassportManagementClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassportManagementClientApplication.class, args);
+    }
+
+}
